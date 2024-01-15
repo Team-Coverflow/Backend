773ec2d47862f63f156867da890b9b381f91a1d8
fakerdeft <whakswp123@naver.com>
2024-01-15 23:22:49 +0900

    (#4)feat: OAuth2 + JWT 로그인 구현 중입니다.

--- src/main/java/com/coverflow/member/domain/Member.java
+++ src/main/java/com/coverflow/member/domain/Member.java
@@ -1,4 +1,51 @@
 package com.coverflow.member.domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Builder
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "member_id")
+    private UUID member_id;
+
+    private String password;
+    private String email;
+    private String gender;
+    private int age;
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType; // KAKAO, NAVER, GOOGLE
+
+    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+    private String refreshToken; // 리프레시 토큰
+
+    public Member() {
+
+    }
+
+    // 유저 권한 설정 메소드
+    public void authorizeMember() {
+        this.role = Role.MEMBER;
+    }
+
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
 }
