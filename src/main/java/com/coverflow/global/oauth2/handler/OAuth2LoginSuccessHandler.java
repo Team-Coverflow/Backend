82a6a25bb39cdb900741e7ea40484f60d1b9b2eb
fakerdeft <whakswp123@naver.com>
2024-01-17 23:35:28 +0900

    (#23)refactor: 불필요한 try-catch 삭제했습니다.

--- src/main/java/com/coverflow/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/coverflow/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -22,24 +22,20 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         log.info("OAuth2 Login 성공!");
-        try {
-            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-            // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
-            if (oAuth2User.getRole() == Role.GUEST) {
-                String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
-                response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-                response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
+        // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
+        if (oAuth2User.getRole() == Role.GUEST) {
+            String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+            response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+            response.sendRedirect("oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
 
-                jwtService.sendAccessAndRefreshToken(response, accessToken, null);
+            jwtService.sendAccessAndRefreshToken(response, accessToken, null);
 //                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
 //                                .orElseThrow(() -> new IllegalArgumentException("이메일에 해당하는 유저가 없습니다."));
 //                findUser.authorizeUser();
-            } else {
-                loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
-            }
-        } catch (Exception e) {
-            throw e;
+        } else {
+            loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
         }
     }
 
