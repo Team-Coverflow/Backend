c6bd9e3bbc81a7caac84c6ea6fcb7463f8da7c72
fakerdeft <whakswp123@naver.com>
2024-01-22 10:42:13 +0900

    (#26)refactor: 닉네임 중복 검증 API 코드 수정했습니다.

--- src/main/java/com/coverflow/member/application/MemberService.java
+++ src/main/java/com/coverflow/member/application/MemberService.java
@@ -1,53 +1,71 @@
 package com.coverflow.member.application;
 
-import com.coverflow.member.domain.Member;
 import com.coverflow.member.domain.MemberRepository;
-import com.coverflow.member.domain.Role;
-import com.coverflow.member.dto.MemberSignUpDTO;
 import com.coverflow.member.dto.request.DuplicationNicknameRequest;
+import com.coverflow.member.dto.request.LoginInfoRequest;
 import com.coverflow.member.dto.response.DuplicationNicknameResponse;
-import jakarta.transaction.Transactional;
+import com.coverflow.member.dto.response.LoginInfoResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class MemberService {
+
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void signUp(final MemberSignUpDTO memberSignUpDTO) throws Exception {
-        if (memberRepository.findByEmail(memberSignUpDTO.getEmail()).isPresent()) {
-            throw new Exception("이미 존재하는 이메일입니다.");
-        }
+//    public void signUp(
+//            final MemberSignUpDTO memberSignUpDTO
+//    ) throws Exception {
+//        if (memberRepository.findByEmail(memberSignUpDTO.getEmail()).isPresent()) {
+//            throw new Exception("이미 존재하는 이메일입니다.");
+//        }
+//
+//        if (memberRepository.findByNickname(memberSignUpDTO.getNickname()).isPresent()) {
+//            throw new Exception("이미 존재하는 닉네임입니다.");
+//        }
+//
+//        final Member member = Member.builder()
+//                .email(memberSignUpDTO.getEmail())
+//                .password(memberSignUpDTO.getPassword())
+//                .nickname(memberSignUpDTO.getNickname())
+//                .age(memberSignUpDTO.getAge())
+//                .role(Role.MEMBER)
+//                .build();
+//
+//        member.passwordEncode(passwordEncoder);
+//        memberRepository.save(member);
+//    }
 
-        if (memberRepository.findByNickname(memberSignUpDTO.getNickname()).isPresent()) {
-            throw new Exception("이미 존재하는 닉네임입니다.");
-        }
+    @Transactional(readOnly = true)
+    public DuplicationNicknameResponse verifyDuplicationNickname(
+            final DuplicationNicknameRequest request
+    ) {
+        final AtomicBoolean result = new AtomicBoolean(false);
 
-        Member member = Member.builder()
-                .email(memberSignUpDTO.getEmail())
-                .password(memberSignUpDTO.getPassword())
-                .nickname(memberSignUpDTO.getNickname())
-                .age(memberSignUpDTO.getAge())
-                .role(Role.MEMBER)
-                .build();
+        memberRepository.findByNickname(request.nickname())
+                .ifPresentOrElse(
+                        member -> result.set(false),
+                        () -> result.set(true)
+                );
 
-        member.passwordEncode(passwordEncoder);
-        memberRepository.save(member);
+//        if (nickname.isPresent()) {
+//            return new DuplicationNicknameResponse(HttpStatus.OK, HttpStatus.OK.toString(), "이미 존재하는 닉네임입니다.");
+//        }
+//        return new DuplicationNicknameResponse(HttpStatus.OK, HttpStatus.OK.toString(), "사용 가능한 닉네임입니다.");
+        return DuplicationNicknameResponse.of(result.get());
     }
 
-    public DuplicationNicknameResponse verifyDuplicationNickname(final DuplicationNicknameRequest request) {
-        final Optional<Member> nickname = memberRepository.findByNickname(request.nickname());
-        
-        if (nickname.isPresent()) {
-            return new DuplicationNicknameResponse(HttpStatus.OK, HttpStatus.OK.toString(), "이미 존재하는 닉네임입니다.");
-        }
-        return new DuplicationNicknameResponse(HttpStatus.OK, HttpStatus.OK.toString(), "사용 가능한 닉네임입니다.");
+    public LoginInfoResponse saveLoginInfo(
+            final LoginInfoRequest request
+    ) {
+        Optional<>
     }
 }
--- src/main/java/com/coverflow/member/presentation/MemberController.java
+++ src/main/java/com/coverflow/member/presentation/MemberController.java
@@ -2,23 +2,33 @@ package com.coverflow.member.presentation;
 
 import com.coverflow.member.application.MemberService;
 import com.coverflow.member.dto.request.DuplicationNicknameRequest;
+import com.coverflow.member.dto.request.LoginInfoRequest;
 import com.coverflow.member.dto.response.DuplicationNicknameResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/member")
 @RestController
 public class MemberController {
+
     private final MemberService memberService;
 
     @GetMapping("/verify-duplication-nickname")
-    public ResponseEntity<DuplicationNicknameResponse> verifyDuplicationNickname(@RequestBody final DuplicationNicknameRequest request) {
-        final DuplicationNicknameResponse duplicationNicknameResponse = memberService.verifyDuplicationNickname(request);
-        return ResponseEntity.ok(duplicationNicknameResponse);
+    public ResponseEntity<DuplicationNicknameResponse> verifyDuplicationNickname(
+            @RequestBody @Valid final DuplicationNicknameRequest request
+    ) {
+        DuplicationNicknameResponse duplicationNicknameResponse = memberService.verifyDuplicationNickname(request);
+        return ResponseEntity.ok().body(duplicationNicknameResponse);
+    }
+
+    @PostMapping("/save-login-info")
+    public ResponseEntity<Void> saveLoginInfo(
+            @RequestBody @Valid final LoginInfoRequest request
+    ) {
+        memberService.saveLoginInfo(request);
+        return ResponseEntity.ok().build();
     }
 }
