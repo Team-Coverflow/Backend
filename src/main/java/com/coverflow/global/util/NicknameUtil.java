d739407a9d4e41e5ce8b7c8033a2287008b06b63
fakerdeft <whakswp123@naver.com>
2024-01-25 20:03:45 +0900

    (#36)refactor: 음식 닉네임 추가했습니다.

--- src/main/java/com/coverflow/global/util/NicknameUtil.java
+++ src/main/java/com/coverflow/global/util/NicknameUtil.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @RequiredArgsConstructor
 public class NicknameUtil {
-    
+
     static MemberRepository memberRepository;
 
     public static String generateRandomNickname() {
@@ -30,14 +30,24 @@ public class NicknameUtil {
                     "따뜻한", "풍족한", "희망찬", "무지한", "고요한", "편안한", "불편한", "웅장한", "조그마한", "창창한", // 90개
                     "빈번한", "드물게", "창조적인", "보수적인", "진보적인", "밝은", "어두운", "포근한", "듬직한", "무서운" // 100개
             );
-            final List<String> secondName = List.of();
-            final List<String> thirdName = List.of();
+            final List<String> secondName = Arrays.asList(
+                    "치즈피자", "파스타", "스테이크", "샐러드", "불고기버거", "초밥", "카레", "치즈김밥", "만두", "라면", // 10개
+                    "볶음밥", "타코", "샌드위치", "치킨", "수프", "스시", "닭갈비", "갈비찜", "곱창", "떡볶이", // 20개
+                    "부대찌개", "치즈카츠", "삼겹살", "라멘", "김치찌개", "감자튀김", "생선구이", "불고기", "오므라이스", "카레라이스", // 30개
+                    "스파게티", "해물찜", "갈비탕", "새우튀김", "매운탕", "베이컨", "소시지", "콩나물국밥", "김치볶음밥", "해물파전", // 40개
+                    "샤브샤브", "훈제오리", "감자전", "삼계탕", "제육볶음", "누룽지", "동태찌개", "불닭볶음면", "잔치국수", "꼬치구이",  // 50개
+                    "새우볶음밥", "닭볶음탕", "냉면", "팟타이", "찜닭", "고등어조림", "해물탕", "쭈꾸미볶음", "소고기국밥", "버팔로윙", // 60개
+                    "라볶이", "떡국", "새우깡", "감자탕", "떡갈비", "부타동", "돼지불백", "김치전", "탕수육", "김치찜", // 70개
+                    "순두부찌개", "된장찌개", "닭꼬치", "치즈돈까스", "왕돈까스", "등심돈까스", "안심돈까스", "등심카츠", "안심카츠", "고구마튀김", // 80개
+                    "김말이", "쫄면", "참치김밥", "삼겹살김밥", "순대국밥", "육개장", "치즈케이크", "미역국", "수육국밥", "갈치조림", // 90개
+                    "대창전골", "곱창전골", "낙곱새", "대창", "모츠나베", "교동", "우동", "치즈버거", "불고기피자", "마라탕", // 100개
+                    "부채살", "목살", "오겹살", "흑임자호빵", "고구마호빵", "팥호빵", "피자호빵", "팥붕어빵", "슈붕어빵", "호떡" // 110개
+            );
 
             Collections.shuffle(firstName);
             Collections.shuffle(secondName);
-            Collections.shuffle(thirdName);
 
-            nickname = firstName.get(0) + " " + secondName.get(0) + " " + thirdName.get(0); // 100*100*100 = 1억개 가능
+            nickname = firstName.get(0) + " " + secondName.get(0); // 100*100 = 1억개 가능
 
             memberRepository.findByNickname(nickname)
                     .ifPresentOrElse(
