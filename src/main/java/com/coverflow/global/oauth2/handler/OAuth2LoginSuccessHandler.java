9d46e34ea6421b9fb9ac3177fedb59c2920d4cbf
fakerdeft <whakswp123@naver.com>
2024-01-25 10:44:42 +0900

    (#36)fix: 쿼리스트링으로 토큰 보내도록 수정했습니다.

--- src/main/java/com/coverflow/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/coverflow/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -1,7 +1,10 @@
 package com.coverflow.global.oauth2.handler;
 
 import com.coverflow.global.jwt.service.JwtService;
+import com.coverflow.global.oauth2.CustomOAuth2User;
+import com.coverflow.member.domain.Member;
 import com.coverflow.member.domain.MemberRepository;
+import com.coverflow.member.domain.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +13,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,16 +36,58 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
             final Authentication authentication
     ) throws IOException {
         log.info("OAuth2 Login 성공!");
-//        final CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        final CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+        // 로그인 성공하면
+        // 리프레쉬 토큰 유무에 관계없이(첫 사용자, 기존 사용자 모두)
+        // 액세스 + 리프레쉬 토큰 발급
+        // 즉, 리프레쉬 토큰은 1회용(보안 강화)
+        final String accessToken = jwtService.createAccessToken(String.valueOf(oAuth2User.getMemberId()));
+        final String refreshToken = jwtService.createRefreshToken();
+        final String targetUrl = createURI(accessToken, refreshToken).toString();
+
+        // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
+        if (oAuth2User.getRole() == Role.GUEST) {
+            final Member member = memberRepository.findByMemberId(oAuth2User.getMemberId())
+                    .orElseThrow(() -> new IllegalArgumentException("일치하는 회원이 없습니다."));
+
+            member.authorizeMember();
+        }
+        response.sendRedirect(targetUrl); // 프론트의 추가 정보 입력 폼으로 리다이렉트
+    }
+
+//    // 소셜 로그인 후
+//    // 리프레쉬 토큰 유무에 관계없이(첫 사용자, 기존 사용자 모두)
+//    // 액세스 + 리프레쉬 토큰 발급
+//    // 즉, 리프레쉬 토큰은 1회용(보안 강화)
+//    private void loginSuccess(
+//            final HttpServletResponse response,
+//            final CustomOAuth2User oAuth2User
+//    ) {
+//        final String accessToken = jwtService.createAccessToken(String.valueOf(oAuth2User.getMemberId()));
+//        final String refreshToken = jwtService.createRefreshToken();
 //
-//        loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
+//        response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+//        response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
 //
-//        // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트
-//        if (oAuth2User.getRole() == Role.GUEST) {
-//            final Member member = memberRepository.findByMemberId(oAuth2User.getMemberId())
-//                    .orElseThrow(() -> new IllegalArgumentException("일치하는 회원이 없습니다."));
-//
-//            member.authorizeMember();
-        response.sendRedirect("/auth/token"); // 프론트의 추가 정보 입력 폼으로 리다이렉트
+//        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+//        jwtService.updateRefreshToken(oAuth2User.getMemberId(), refreshToken);
+//    }
+
+    // JWT를 쿼리 파라미터로 담아 리다이렉트
+    private URI createURI(String accessToken, String refreshToken) {
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("access_token", accessToken);
+        queryParams.add("refresh_token", refreshToken);
+
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("https")
+                .host("coverflow.co.kr")
+                .path("/auth/token")
+                .queryParams(queryParams)
+                .build()
+                .encode()
+                .toUri();
     }
 }
