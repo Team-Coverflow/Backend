3a662338b6b9879586a4bf869e1947438ff4af89
fakerdeft <whakswp123@naver.com>
2024-01-25 13:20:18 +0900

    (#36)refactor: 주석 수정했습니다.

--- src/main/java/com/coverflow/global/login/service/LoginService.java
+++ src/main/java/com/coverflow/global/login/service/LoginService.java
@@ -1,5 +1,6 @@
 package com.coverflow.global.login.service;
 
+import com.coverflow.member.domain.Member;
 import com.coverflow.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,13 +21,13 @@ public class LoginService implements UserDetailsService {
     public UserDetails loadUserByUsername(
             final String email
     ) throws UsernameNotFoundException {
-//        Member user = memberRepository.findByEmail(email)
-//                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
-//
-//        return org.springframework.security.core.userdetails.User.builder()
-//                .username(user.getEmail())
-//                .password(user.getPassword())
-//                .roles(user.getRole().name())
-//                .build();
+        Member user = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
     }
 }
