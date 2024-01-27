b86a6aa7a37dd74446cd5faa6e3175551603f515
아맞다우산 <whakswp123@naver.com>
2024-01-27 15:51:15 +0900

    운영 서버 배포 중입니다.

--- src/main/java/com/coverflow/global/config/SecurityConfig.java
+++ src/main/java/com/coverflow/global/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @Configuration
