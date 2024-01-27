d339401f7a6e970f1d47b1abb0b898a467f831cc
아맞다우산 <whakswp123@naver.com>
2024-01-27 12:03:34 +0900

    fix: cors 포트 번호 추가했습니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 모든 uri에 대해 http://localhost:3000 도메인은 접근을 허용한다.
+        // 모든 uri에 대해 http://localhost:3000, http://localhost:3001 도메인은 접근을 허용한다.
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://localhost:3001")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
