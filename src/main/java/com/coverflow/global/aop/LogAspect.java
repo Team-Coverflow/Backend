b4a5ade4120ac2d40b7d4cdae963ad5540817e1d
fakerdeft <whakswp123@naver.com>
2024-02-03 01:57:20 +0900

    (#58)refactor: 로그 수정했습니다.

--- src/main/java/com/coverflow/global/aop/LogAspect.java
+++ src/main/java/com/coverflow/global/aop/LogAspect.java
@@ -2,7 +2,6 @@ package com.coverflow.global.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
@@ -14,9 +13,9 @@ import java.lang.reflect.Method;
 @Aspect
 public class LogAspect {
 
-    @Pointcut("execution(* com.coverflow..*(..))")
-    public void all() {
-    }
+//    @Pointcut("execution(* com.coverflow..*(..))")
+//    public void all() {
+//    }
 
     @Pointcut("execution(* com.coverflow..*Controller.*(..))")
     public void controller() {
@@ -26,49 +25,59 @@ public class LogAspect {
     public void service() {
     }
 
-    @Around("all()")
-    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-        try {
-            return joinPoint.proceed();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            log.info("log = {}", joinPoint.getSignature());
-            log.info("timeMs = {}", timeMs);
-        }
-    }
+//    @Around("all()")
+//    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long start = System.currentTimeMillis();
+//        try {
+//            return joinPoint.proceed();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            log.info("log = {}", joinPoint.getSignature());
+//            log.info("timeMs = {}", timeMs);
+//        }
+//    }
 
     // 특정 조인포인트에서 수행될 부가기능을 정리
     @Before("controller() || service()")
     public void beforeLogic(JoinPoint joinPoint) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        log.info("method = {}", method.getName());
+        Method method = getMethod(joinPoint);
+        log.info("====== method = {} ======", method.getName());
 
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
             if (arg != null) {
-                log.info("type = {}", arg.getClass().getSimpleName());
-                log.info("value = {}", arg);
+                log.info("parameter type = {}", arg.getClass().getSimpleName());
+                log.info("parameter value = {}", arg);
             }
 
         }
     }
 
-    @After("controller() || service()")
+    @AfterReturning("controller() || service()")
     public void afterLogic(JoinPoint joinPoint) {
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
-        log.info("method = {}", method.getName());
+        Method method = getMethod(joinPoint);
+        log.info("====== method = {} ======", method.getName());
 
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
             if (arg != null) {
-                log.info("type = {}", arg.getClass().getSimpleName());
-                log.info("value = {}", arg);
+                log.info("return type = {}", arg.getClass().getSimpleName());
+                log.info("return value = {}", arg);
             }
 
         }
+    }
+
+    @AfterThrowing(pointcut = "controller()", throwing = "e")
+    public void afterThrowingLogging(JoinPoint joinPoint, Exception e) {
+        log.error("### Occured error in request {}", joinPoint.getSignature().toShortString());
+        log.error("\t{}", e.getMessage());
+    }
+
+    // JoinPoint로 메서드 정보 가져오기
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
     }
 }
