d3b1093eccf20622aada02a86d7ef842e523367f
fakerdeft <whakswp123@naver.com>
2024-02-04 23:50:22 +0900

    (#59)feat: 회사 검색 결과 response 추가했습니다.

--- src/main/java/com/coverflow/company/application/CompanyService.java
+++ src/main/java/com/coverflow/company/application/CompanyService.java
@@ -1,11 +1,21 @@
 package com.coverflow.company.application;
 
+import com.coverflow.company.dto.response.FindCompanyResponse;
+import com.coverflow.company.infrastructure.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public List<FindCompanyResponse> findCompany() {
+        return
+    }
 }
--- src/main/java/com/coverflow/company/presentation/CompanyController.java
+++ src/main/java/com/coverflow/company/presentation/CompanyController.java
@@ -1,9 +1,32 @@
 package com.coverflow.company.presentation;
 
+import com.coverflow.company.application.CompanyService;
+import com.coverflow.company.dto.response.FindCompanyResponse;
+import com.coverflow.global.response.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("/api/company")
 @RequiredArgsConstructor
 @RestController
 public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @GetMapping("/search")
+    public ResponseEntity<ResponseHandler<List<FindCompanyResponse>>> findCompany() {
+        return ResponseEntity.ok(
+                ResponseHandler.<List<FindCompanyResponse>>builder()
+                        .statusCode(HttpStatus.OK)
+                        .message("회사 검색에 성공했습니다.")
+                        .data(companyService.findCompany())
+                        .build()
+        );
+    }
 }
