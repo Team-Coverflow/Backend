11125093a8ea7bb9964bbab03b5fd663302557d2
아맞다우산 <whakswp123@naver.com>
2024-02-05 21:49:32 +0900

    개발 서버 리액트 CORS 허용했습니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 uri에 대해 http://localhost:3000, http://localhost:3001 도메인은 접근을 허용한다.
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001","http://15.165.1.48","http://15.165.1.48:3001")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
