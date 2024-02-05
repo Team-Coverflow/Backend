ae5922135562fe7bdeb9d8b8903de599ec011b87
fakerdeft <whakswp123@naver.com>
2024-02-05 15:18:09 +0900

    (#59)feat: 회사 검색 기능 구현했습니다.

--- src/main/java/com/coverflow/company/presentation/CompanyController.java
+++ src/main/java/com/coverflow/company/presentation/CompanyController.java
@@ -2,12 +2,14 @@ package com.coverflow.company.presentation;
 
 import com.coverflow.company.application.CompanyService;
 import com.coverflow.company.dto.response.FindCompanyResponse;
-import com.coverflow.global.response.ResponseHandler;
+import com.coverflow.global.handler.ResponseHandler;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,14 +21,29 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
-    @GetMapping("/search")
-    public ResponseEntity<ResponseHandler<List<FindCompanyResponse>>> findCompany() {
-        return ResponseEntity.ok(
-                ResponseHandler.<List<FindCompanyResponse>>builder()
+    @GetMapping("/auto-complete")
+    public ResponseEntity<ResponseHandler<List<FindCompanyResponse>>> findCompaniesByName(
+            final @RequestParam @Valid String name
+    ) {
+        return ResponseEntity.ok()
+                .body(ResponseHandler.<List<FindCompanyResponse>>builder()
                         .statusCode(HttpStatus.OK)
-                        .message("회사 검색에 성공했습니다.")
-                        .data(companyService.findCompany())
+                        .message("회사 리스트 검색에 성공했습니다.")
+                        .data(companyService.findCompaniesByName(name))
                         .build()
-        );
+                );
+    }
+
+    @GetMapping("/find-company")
+    public ResponseEntity<ResponseHandler<FindCompanyResponse>> findCompanyByName(
+            final @RequestParam @Valid String name
+    ) {
+        return ResponseEntity.ok()
+                .body(ResponseHandler.<FindCompanyResponse>builder()
+                        .statusCode(HttpStatus.OK)
+                        .message("회사 조회에 성공했습니다.")
+                        .data(companyService.findCompanyByName(name))
+                        .build()
+                );
     }
 }
