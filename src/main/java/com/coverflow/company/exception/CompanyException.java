c825adfb942058706eb0d6241fb0eb6bb08f0e0c
fakerdeft <whakswp123@naver.com>
2024-02-05 15:17:50 +0900

    (#59)feat: 회사 조회 예외 구현했습니다.

--- src/main/java/com/coverflow/company/exception/CompanyException.java
+++ src/main/java/com/coverflow/company/exception/CompanyException.java
@@ -1,4 +1,15 @@
 package com.coverflow.company.exception;
 
 public class CompanyException extends RuntimeException {
+
+    public CompanyException(final String message) {
+        super(message);
+    }
+
+    public static class CompanyNotFoundException extends CompanyException {
+
+        public CompanyNotFoundException(final String name) {
+            super(String.format("회사가 존재하지 않습니다. - request info { name : %s }", name));
+        }
+    }
 }
