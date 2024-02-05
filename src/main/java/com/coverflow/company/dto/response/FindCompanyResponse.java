c68825899692a182238c8202ada87bc1a3bc8a12
fakerdeft <whakswp123@naver.com>
2024-02-05 14:38:04 +0900

    (#59)feat: 회사 조회 응답 추가했습니다.

--- src/main/java/com/coverflow/company/dto/response/FindCompanyResponse.java
+++ src/main/java/com/coverflow/company/dto/response/FindCompanyResponse.java
@@ -1,6 +1,19 @@
 package com.coverflow.company.dto.response;
 
-public record FindCompanyResponse(
+import com.coverflow.company.domain.Company;
 
+public record FindCompanyResponse(
+        String name,
+        String type,
+        String address,
+        String establishment
 ) {
+    public static FindCompanyResponse of(Company company) {
+        return new FindCompanyResponse(
+                company.getName(),
+                company.getType(),
+                company.getCity() + " " + company.getDistrict(),
+                company.getEstablishment()
+        );
+    }
 }
