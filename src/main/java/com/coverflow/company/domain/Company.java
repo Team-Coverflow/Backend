f9052adc811bbe46737b67cbd60982230cbb22f1
fakerdeft <whakswp123@naver.com>
2024-02-05 13:06:29 +0900

    (#59)feat: 회사 엔티티 구현했습니다.

--- src/main/java/com/coverflow/company/domain/Company.java
+++ src/main/java/com/coverflow/company/domain/Company.java
@@ -1,4 +1,25 @@
 package com.coverflow.company.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_company")
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String type;
+    @Column
+    private String address;
+    @Column
+    private String establishment;
 }
