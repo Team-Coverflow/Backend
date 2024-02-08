1a3b13230f6a4d25e7cdca05f7db93452259703f
fakerdeft <whakswp123@naver.com>
2024-02-08 19:33:55 +0900

    (#37)fix: 함수 이중 호출 수정했습니다.

--- src/main/java/com/coverflow/visitor/domain/Visitor.java
+++ src/main/java/com/coverflow/visitor/domain/Visitor.java
@@ -13,11 +13,11 @@ public class Visitor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id; // 방문자 수 고유 번호
     @Column
-    private String today;
+    private String today; // 오늘 날짜
     @Column
-    private int count;
+    private int count; // 방문자 수
 
     public void updateVisitors(final int count) {
         this.count = count;
