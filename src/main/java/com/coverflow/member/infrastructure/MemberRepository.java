8879e32c46105e9a2483a311af5d56088e5e2622
fakerdeft <whakswp123@naver.com>
2024-02-08 14:19:33 +0900

    (#37)chore: 띄어쓰기 했습니다.

--- src/main/java/com/coverflow/member/infrastructure/MemberRepository.java
+++ src/main/java/com/coverflow/member/infrastructure/MemberRepository.java
@@ -10,7 +10,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
-    Optional<Member> findByMemberId(UUID memberId);
+
+    Optional<Member> findByMemberId(final UUID memberId);
 
     @Query(value = "SELECT m FROM Member m ORDER BY m.createdAt ASC")
     Optional<List<Member>> findAllMembers();
@@ -23,10 +24,13 @@ public interface MemberRepository extends JpaRepository<Member, UUID> {
 
     /**
      * 소셜 타입과 소셜의 식별값으로 회원 찾는 메소드
-     * 정보 제공을 동의한 순간 DB에 저장해야하지만, 아직 추가 정보(사는 도시, 나이 등)를 입력받지 않았으므로
+     * 정보 제공을 동의한 순간 DB에 저장해야하지만, 아직 추가 정보(태그, 연령대, 성별 등)를 입력받지 않았으므로
      * 유저 객체는 DB에 있지만, 추가 정보가 빠진 상태이다.
-     * 따라서 추가 정보를 입력받아 회원 가입을 진행할 때 소셜 타입, 식별자로 해당 회원을 찾기 위한 메소드
+     * 따라서 추가 정보를 입력받아 진행할 때 소셜 타입, 식별자로 해당 회원을 찾기 위한 메소드
      */
-    Optional<Member> findBySocialTypeAndSocialId(final SocialType socialType, final String socialId);
+    Optional<Member> findBySocialTypeAndSocialId(
+            final SocialType socialType,
+            final String socialId
+    );
 
 }
