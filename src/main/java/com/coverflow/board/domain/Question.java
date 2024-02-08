a8ceeafeee380a2d2154f706a5ce6b37438f0c33
fakerdeft <whakswp123@naver.com>
2024-02-08 10:38:19 +0900

    (#37)feat: 질문 엔티티 구현했습니다.

---
+++ src/main/java/com/coverflow/board/domain/Question.java
@@ -0,0 +1,24 @@
+package com.coverflow.board.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+    @Column
+    private int count;
+    @Column
+    private String status;
+}
