13f7b6435d4b336bcd1e99db5d32c7e98d8b5c8c
아맞다우산 <whakswp123@naver.com>
2024-02-08 09:24:36 +0900

    CORS 허용할 URL 수정했습니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 모든 uri에 대해 http://localhost:3000, http://localhost:3001 도메인은 접근을 허용한다.
+        // 모든 uri에 대해 특정 도메인은 접근을 허용한다.
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001","http://15.165.1.48","http://15.165.1.48:3001","http://172.17.0.3:3001")
+                .allowedOrigins("http://localhost:3001","http://15.165.1.48")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
