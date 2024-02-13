ed1bcbfecc19e0603e84a36471f2a7b4d5050a12
아맞다우산 <whakswp123@naver.com>
2024-02-13 16:36:00 +0900

    테스트 중입니다.

--- src/main/java/com/coverflow/global/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/coverflow/global/jwt/filter/JwtAuthenticationFilter.java
@@ -47,7 +47,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             final HttpServletResponse response,
             final FilterChain filterChain
     ) throws ServletException, IOException {
+        System.out.println("요청1: "+request);
         System.out.println("요청123: "+request.getRequestURI());
+        System.out.println("액토: "+request.getHeaderNames());
+        System.out.println("액토: "+request.getHeader(accessHeader));
         if (request.getRequestURI().equals(NO_CHECK_URL)) {
             filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
             return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
