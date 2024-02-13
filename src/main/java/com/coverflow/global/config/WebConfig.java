c0c27f7ef83f34f6e623abb1402a83bebdcc1334
아맞다우산 <whakswp123@naver.com>
2024-02-13 18:28:47 +0900

    cors 해결 중입니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 uri에 대해 특정 도메인은 접근을 허용한다.
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://localhost:3001","http://15.165.1.48","http://15.165.1.48:3001","http://172.17.0.3:3001")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedHeaders("Content-Type", "Authorization", "Referer", "User-Agent")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
