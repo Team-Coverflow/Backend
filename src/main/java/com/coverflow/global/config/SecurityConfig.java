f717365671fd9389467cb0a59b2ce82bd7768d4d
아맞다우산 <whakswp123@naver.com>
2024-02-13 21:23:00 +0900

    cors 해결 중입니다.

--- src/main/java/com/coverflow/global/config/SecurityConfig.java
+++ src/main/java/com/coverflow/global/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
