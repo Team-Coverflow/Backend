4e387a4d94349fd8029b5038335638f8ba90f19c
아맞다우산 <whakswp123@naver.com>
2024-02-13 16:20:03 +0900

    cors 테스트 중입니다.

--- src/main/java/com/coverflow/global/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/coverflow/global/jwt/filter/JwtAuthenticationFilter.java
@@ -47,7 +47,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             final HttpServletResponse response,
             final FilterChain filterChain
     ) throws ServletException, IOException {
-        System.out.println("요청: ":request.getRequestURI());
+        System.out.println("요청123: "+request.getRequestURI());
         if (request.getRequestURI().equals(NO_CHECK_URL)) {
             filterChain.doFilter(request, response); // "/login" 요청이 들어오면, 다음 필터 호출
             return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
