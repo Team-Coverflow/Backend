2e992b887b9c7802fab3b35f177f8b0a1ee14ea0
아맞다우산 <whakswp123@naver.com>
2024-02-13 18:09:26 +0900

    cors 테스트 중입니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         // 모든 uri에 대해 특정 도메인은 접근을 허용한다.
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:3000", "http://localhost:3001","http://15.165.1.48","http://15.165.1.48:3001","http://172.17.0.3:3001")
-                .allowedMethods("GET", "POST")
+                .allowedMethods("*")
                 .allowedHeaders("Content-Type", "Authorization", "Referer", "User-Agent")
                 .allowCredentials(true);
     }
