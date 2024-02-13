7974b7f41c737431f18dcb7615d38b74ce1ed5c9
아맞다우산 <whakswp123@naver.com>
2024-02-13 15:54:13 +0900

    url 테스트 중입니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.coverflow.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
