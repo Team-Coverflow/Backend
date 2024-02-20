97436e812a3ded929d50ac09ddb883e48a8e4609
fakerdeft <whakswp123@naver.com>
2024-02-21 05:28:27 +0900

    (#102)feat: sse 구현 중입니다.

--- src/main/java/com/coverflow/notification/application/NotificationService.java
+++ src/main/java/com/coverflow/notification/application/NotificationService.java
@@ -6,21 +6,59 @@ import com.coverflow.notification.dto.response.FindNotificationResponse;
 import com.coverflow.notification.exception.NotificationException;
 import com.coverflow.notification.infrastructure.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    /**
+     * [보낼 메시지 담는 메서드]
+     */
+    public SseEmitter add(SseEmitter emitter) {
+        emitters.add(emitter);
+        log.info("new emitter added: {}", emitter);
+        log.info("emitter list size: {}", emitters.size());
+        log.info("emitter list: {}", emitters);
+        emitter.onCompletion(() -> {
+            log.info("onCompletion callback");
+            emitters.remove(emitter);
+        });
+        emitter.onTimeout(() -> {
+            log.info("onTimeout callback");
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
+    public void alert() {
+        emitters.forEach(emitter -> {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("alert")
+                        .data("count"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     /**
      * [알림 조회 메서드]
@@ -58,4 +96,5 @@ public class NotificationService {
         LocalDateTime date = LocalDateTime.now().minusDays(30);
         notificationRepository.deleteByCreatedAt(date);
     }
+
 }
--- src/main/java/com/coverflow/notification/presentation/NotificationController.java
+++ src/main/java/com/coverflow/notification/presentation/NotificationController.java
@@ -2,47 +2,75 @@ package com.coverflow.notification.presentation;
 
 import com.coverflow.global.handler.ResponseHandler;
 import com.coverflow.notification.application.NotificationService;
-import com.coverflow.notification.dto.request.UpdateNotificationRequest;
-import com.coverflow.notification.dto.response.FindNotificationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.List;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/notification")
 @RestController
 public class NotificationController {
 
+    //    private final SseEmitter sseEmitters;
     private final NotificationService notificationService;
 
-    @GetMapping("/find-notification")
-    public ResponseEntity<ResponseHandler<List<FindNotificationResponse>>> findNotification(
-            @AuthenticationPrincipal final UserDetails userDetails
-    ) {
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<ResponseHandler<SseEmitter>> connect() {
+        final SseEmitter emitter = new SseEmitter();
+        
+        notificationService.add(emitter);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data("connected!"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return ResponseEntity.ok()
-                .body(ResponseHandler.<List<FindNotificationResponse>>builder()
+                .body(ResponseHandler.<SseEmitter>builder()
                         .statusCode(HttpStatus.OK)
-                        .message("알림 조회에 성공했습니다.")
-                        .data(notificationService.findNotification(userDetails.getUsername()))
+                        .message("알림 서버 연결 성공했습니다.")
+                        .data(emitter)
                         .build()
                 );
     }
 
-    @PostMapping("/update-notification")
-    public ResponseEntity<ResponseHandler<Void>> updateNotification(
-            @RequestBody final List<UpdateNotificationRequest> requests
-    ) {
-        notificationService.updateNotification(requests);
-        return ResponseEntity.ok()
-                .body(ResponseHandler.<Void>builder()
-                        .statusCode(HttpStatus.OK)
-                        .message("알림 변경에 성공했습니다.")
-                        .build()
-                );
+    @PostMapping("/count")
+    public ResponseEntity<Void> count() {
+        return ResponseEntity.ok().build();
     }
+
+//    @GetMapping("/find-notification")
+//    public ResponseEntity<ResponseHandler<List<FindNotificationResponse>>> findNotification(
+//            @AuthenticationPrincipal final UserDetails userDetails
+//    ) {
+//        return ResponseEntity.ok()
+//                .body(ResponseHandler.<List<FindNotificationResponse>>builder()
+//                        .statusCode(HttpStatus.OK)
+//                        .message("알림 조회에 성공했습니다.")
+//                        .data(notificationService.findNotification(userDetails.getUsername()))
+//                        .build()
+//                );
+//    }
+//
+//    @PostMapping("/update-notification")
+//    public ResponseEntity<ResponseHandler<Void>> updateNotification(
+//            @RequestBody final List<UpdateNotificationRequest> requests
+//    ) {
+//        notificationService.updateNotification(requests);
+//        return ResponseEntity.ok()
+//                .body(ResponseHandler.<Void>builder()
+//                        .statusCode(HttpStatus.OK)
+//                        .message("알림 변경에 성공했습니다.")
+//                        .build()
+//                );
+//    }
 }
