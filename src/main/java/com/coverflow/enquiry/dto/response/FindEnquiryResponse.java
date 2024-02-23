75d9617f1985cf5db52b6c42eb97cd97b70ff1b2
fakerdeft <whakswp123@naver.com>
2024-02-23 14:16:38 +0900

    (#107)feat: 문의 응답 객체 수정했습니다.

--- src/main/java/com/coverflow/enquiry/dto/response/FindEnquiryResponse.java
+++ src/main/java/com/coverflow/enquiry/dto/response/FindEnquiryResponse.java
@@ -1,5 +1,7 @@
 package com.coverflow.enquiry.dto.response;
 
+import com.coverflow.enquiry.domain.Enquiry;
+
 import java.util.UUID;
 
 public record FindEnquiryResponse(
@@ -8,4 +10,13 @@ public record FindEnquiryResponse(
         String status,
         UUID memberId
 ) {
+
+    public static FindEnquiryResponse from(final Enquiry enquiry) {
+        return new FindEnquiryResponse(
+                enquiry.getId(),
+                enquiry.getContent(),
+                enquiry.getStatus(),
+                enquiry.getMember().getId()
+        );
+    }
 }
