02488e2592fc02a86eca1beb8bf9b03a3e96290b
fakerdeft <whakswp123@naver.com>
2024-02-23 09:27:04 +0900

    (#106)feat: 신고 엔티티 구현했습니다.

---
+++ src/main/java/com/coverflow/report/domain/Report.java
@@ -0,0 +1,37 @@
+package com.coverflow.report.domain;
+
+import com.coverflow.global.entity.BaseTimeEntity;
+import com.coverflow.member.domain.Member;
+import com.coverflow.question.domain.Answer;
+import com.coverflow.question.domain.Question;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_report")
+public class Report extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 신고 고유 번호
+    @Column
+    private String content; // 내용
+    @Column
+    private String status; // 상태
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 작성자 정보
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Question question; // 질문 정보
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Answer answer; // 답변 정보
+}
