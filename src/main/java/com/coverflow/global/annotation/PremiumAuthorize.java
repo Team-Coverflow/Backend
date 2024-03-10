6405781c772399c217f5b8c9677db81ac29ebb63
fakerdeft <whakswp123@naver.com>
2024-03-11 00:32:59 +0900

    (#131)fix: 프리미엄 권한 애노테이션 수정했습니다.

---
+++ src/main/java/com/coverflow/global/annotation/PremiumAuthorize.java
@@ -0,0 +1,14 @@
+package com.coverflow.global.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('PREMIUM')")
+public @interface PremiumAuthorize {
+}
