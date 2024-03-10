dab60a48f03b769028fcd60a1848f7f0f6a2c0e1
fakerdeft <whakswp123@naver.com>
2024-03-10 22:43:20 +0900

    fix: post 요청 파라미터 수정했습니다.

--- src/main/java/com/coverflow/company/dto/request/SaveCompanyRequest.java
+++ src/main/java/com/coverflow/company/dto/request/SaveCompanyRequest.java
@@ -1,12 +1,9 @@
 package com.coverflow.company.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
 
 public record SaveCompanyRequest(
 
-        @Positive
-        long companyId,
         @NotBlank
         String name,
         @NotBlank
