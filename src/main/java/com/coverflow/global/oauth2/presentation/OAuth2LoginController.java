530749d17ed7b04f39a5f6eec24af1c7b35e3511
fakerdeft <whakswp123@naver.com>
2024-03-12 10:40:17 +0900

    (#143)fix: RequestParam에 회원 권한 추가했습니다.

--- src/main/java/com/coverflow/global/oauth2/presentation/OAuth2LoginController.java
+++ src/main/java/com/coverflow/global/oauth2/presentation/OAuth2LoginController.java
@@ -22,12 +22,15 @@ public class OAuth2LoginController {
 
     @GetMapping("/token")
     public ResponseEntity<ResponseHandler<Void>> code(
-            @RequestParam @NotBlank final String code
+            @RequestParam @NotBlank final String code,
+            @RequestParam @NotBlank final String role
+
     ) {
         log.info("인가 코드 수신 성공 => {}", code);
+        log.info("회원 권한 수신 성공 => {}", role);
 
         // '/'를 구분자로 사용하여 문자열을 분리
-        String[] tokens = oAuth2LoginService.getToken(code).split("/");
+        String[] tokens = oAuth2LoginService.getToken(code, role).split("/");
 
         // 배열의 각 요소에 접근
         String extractedAccessToken = tokens[0];
