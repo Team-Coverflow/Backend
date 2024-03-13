a75bf330933cdb65fca4acb66cf3ca29664769e7
아맞다우산 <whakswp123@naver.com>
2024-03-13 12:20:26 +0900

    fix: cors 설정에 헤더 관련 추가했습니다.

--- src/main/java/com/coverflow/global/config/WebConfig.java
+++ src/main/java/com/coverflow/global/config/WebConfig.java
@@ -12,7 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         // 모든 uri에 대해 특정 도메인은 접근을 허용한다.
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://15.165.1.48", "http://15.165.1.48:3001", "http://172.17.0.3:3001")
-                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .exposedHeaders("Authorization")
+                .exposedHeaders("Authorization-refresh")
                 .allowCredentials(true);
     }
 }
