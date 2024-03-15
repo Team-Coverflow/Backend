d1f4467b358b7004e039e62610b1f61e5f632ec8
fakerdeft <whakswp123@naver.com>
2024-03-15 17:41:05 +0900

    (#149)feat: 질문 등록 시 태그, 카테고리 추가했습니다.

--- src/main/java/com/coverflow/question/dto/request/SaveQuestionRequest.java
+++ src/main/java/com/coverflow/question/dto/request/SaveQuestionRequest.java
@@ -1,9 +1,15 @@
 package com.coverflow.question.dto.request;
 
+import com.coverflow.question.domain.QuestionCategory;
+import com.coverflow.question.domain.QuestionTag;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 public record SaveQuestionRequest(
+        @NotBlank
+        QuestionTag questionTag,
+        @NotBlank
+        QuestionCategory questionCategory,
         @NotBlank
         String title,
         @NotBlank
