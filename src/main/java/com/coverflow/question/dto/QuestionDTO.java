f8f6b8f6d4ca9c2f08e6a32a9d0b5e821b043dd1
fakerdeft <whakswp123@naver.com>
2024-03-15 17:15:09 +0900

    (#149)feat: 질문 응답에 태그, 카테고리 추가했습니다.

--- src/main/java/com/coverflow/question/dto/QuestionDTO.java
+++ src/main/java/com/coverflow/question/dto/QuestionDTO.java
@@ -1,6 +1,8 @@
 package com.coverflow.question.dto;
 
 import com.coverflow.question.domain.Question;
+import com.coverflow.question.domain.QuestionCategory;
+import com.coverflow.question.domain.QuestionTag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,8 @@ public class QuestionDTO {
     private String questionerTag;
     private String questionTitle;
     private String questionContent;
+    private QuestionTag questionTag;
+    private QuestionCategory questionCategory;
     private int questionViewCount;
     private int answerCount;
     private int reward;
@@ -29,6 +33,8 @@ public class QuestionDTO {
                 question.getMember().getTag(),
                 question.getTitle(),
                 question.getContent(),
+                question.getQuestionTag(),
+                question.getQuestionCategory(),
                 question.getViewCount(),
                 question.getAnswerCount(),
                 question.getReward(),
--- src/main/java/com/coverflow/question/dto/response/FindQuestionResponse.java
+++ src/main/java/com/coverflow/question/dto/response/FindQuestionResponse.java
@@ -1,15 +1,20 @@
 package com.coverflow.question.dto.response;
 
 import com.coverflow.question.domain.Question;
+import com.coverflow.question.domain.QuestionCategory;
+import com.coverflow.question.domain.QuestionTag;
 import com.coverflow.question.dto.AnswerDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record FindQuestionResponse(
+        String companyName,
         long questionId,
         String questionTitle,
         String questionContent,
+        QuestionTag questionTag,
+        QuestionCategory questionCategory,
         int viewCount,
         int answerCount,
         int reward,
@@ -25,9 +30,12 @@ public record FindQuestionResponse(
             final int totalPages,
             final List<AnswerDTO> answers) {
         return new FindQuestionResponse(
+                question.getCompany().getName(),
                 question.getId(),
                 question.getTitle(),
                 question.getContent(),
+                question.getQuestionTag(),
+                question.getQuestionCategory(),
                 question.getViewCount(),
                 question.getAnswerCount(),
                 question.getReward(),
