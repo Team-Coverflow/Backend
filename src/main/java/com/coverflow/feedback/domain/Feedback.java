91385f7151066142d1a7427722ff9d36a75c6031
fakerdeft <whakswp123@naver.com>
2024-03-16 15:59:48 +0900

    (#150)feat: 피드백 엔티티 구현했습니다.

--- src/main/java/com/coverflow/feedback/domain/Feedback.java
+++ src/main/java/com/coverflow/feedback/domain/Feedback.java
@@ -1,4 +1,20 @@
 package com.coverflow.feedback.domain;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_company")
 public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 피드백 고유 번호
+    @Column
+    private String content; // 피드백 내용
 }
