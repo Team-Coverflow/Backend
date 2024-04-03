3950bc9658615491864bd66d366808c989b026c3
fakerdeft <whakswp123@naver.com>
2024-04-03 23:39:24 +0900

    (#203)feat: QueryDSL 설정했습니다.

---
+++ src/main/generated/com/coverflow/inquiry/domain/QInquiry.java
@@ -0,0 +1,67 @@
+package com.coverflow.inquiry.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QInquiry is a Querydsl query type for Inquiry
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QInquiry extends EntityPathBase<Inquiry> {
+
+    private static final long serialVersionUID = 273828602L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QInquiry inquiry = new QInquiry("inquiry");
+
+    public final com.coverflow.global.entity.QBaseTimeEntity _super = new com.coverflow.global.entity.QBaseTimeEntity(this);
+
+    public final StringPath answer = createString("answer");
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final EnumPath<InquiryStatus> inquiryStatus = createEnum("inquiryStatus", InquiryStatus.class);
+
+    public final com.coverflow.member.domain.QMember member;
+
+    public final StringPath title = createString("title");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QInquiry(String variable) {
+        this(Inquiry.class, forVariable(variable), INITS);
+    }
+
+    public QInquiry(Path<? extends Inquiry> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QInquiry(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QInquiry(PathMetadata metadata, PathInits inits) {
+        this(Inquiry.class, metadata, inits);
+    }
+
+    public QInquiry(Class<? extends Inquiry> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new com.coverflow.member.domain.QMember(forProperty("member")) : null;
+    }
+
+}
+
---
+++ src/main/generated/com/coverflow/question/domain/QQuestion.java
@@ -0,0 +1,82 @@
+package com.coverflow.question.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QQuestion is a Querydsl query type for Question
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QQuestion extends EntityPathBase<Question> {
+
+    private static final long serialVersionUID = -1195621410L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QQuestion question = new QQuestion("question");
+
+    public final com.coverflow.global.entity.QBaseTimeEntity _super = new com.coverflow.global.entity.QBaseTimeEntity(this);
+
+    public final NumberPath<Integer> answerCount = createNumber("answerCount", Integer.class);
+
+    public final ListPath<Answer, QAnswer> answers = this.<Answer, QAnswer>createList("answers", Answer.class, QAnswer.class, PathInits.DIRECT2);
+
+    public final com.coverflow.company.domain.QCompany company;
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final com.coverflow.member.domain.QMember member;
+
+    public final StringPath questionCategory = createString("questionCategory");
+
+    public final EnumPath<QuestionStatus> questionStatus = createEnum("questionStatus", QuestionStatus.class);
+
+    public final EnumPath<QuestionTag> questionTag = createEnum("questionTag", QuestionTag.class);
+
+    public final ListPath<com.coverflow.report.domain.Report, com.coverflow.report.domain.QReport> reports = this.<com.coverflow.report.domain.Report, com.coverflow.report.domain.QReport>createList("reports", com.coverflow.report.domain.Report.class, com.coverflow.report.domain.QReport.class, PathInits.DIRECT2);
+
+    public final NumberPath<Integer> reward = createNumber("reward", Integer.class);
+
+    public final StringPath title = createString("title");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final NumberPath<Integer> viewCount = createNumber("viewCount", Integer.class);
+
+    public QQuestion(String variable) {
+        this(Question.class, forVariable(variable), INITS);
+    }
+
+    public QQuestion(Path<? extends Question> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QQuestion(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QQuestion(PathMetadata metadata, PathInits inits) {
+        this(Question.class, metadata, inits);
+    }
+
+    public QQuestion(Class<? extends Question> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.company = inits.isInitialized("company") ? new com.coverflow.company.domain.QCompany(forProperty("company")) : null;
+        this.member = inits.isInitialized("member") ? new com.coverflow.member.domain.QMember(forProperty("member")) : null;
+    }
+
+}
+
