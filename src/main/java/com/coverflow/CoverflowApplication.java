963190d1c62ed73dffc70a28f57100da966f9ea4
fakerdeft <whakswp123@naver.com>
2024-04-09 15:54:38 +0900

    (#205)refactor: 허용 URL 환경 설정 파일에서 설정하도록 수정했습니다.

--- src/main/java/com/coverflow/CoverflowApplication.java
+++ src/main/java/com/coverflow/CoverflowApplication.java
@@ -1,13 +1,16 @@
 package com.coverflow;
 
+import com.coverflow.global.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
 public class CoverflowApplication {
 
     public static void main(String[] args) {
