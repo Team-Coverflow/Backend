d071d8b3c7f0088ca6e63706b10516e99bb59951
fakerdeft <whakswp123@naver.com>
2024-04-11 01:19:11 +0900

    (#207)feat: 알림 서비스 인터페이스 구현했습니다.

--- src/main/java/com/coverflow/notification/application/NotificationService.java
+++ src/main/java/com/coverflow/notification/application/NotificationService.java
@@ -1,10 +1,36 @@
 package com.coverflow.notification.application;
 
 import com.coverflow.notification.domain.Notification;
+import com.coverflow.notification.dto.request.UpdateNotificationRequest;
+import com.coverflow.notification.dto.response.FindNotificationResponse;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 public interface NotificationService {
 
+    /**
+     * 실시간 알림 연결
+     */
+    SseEmitter connect(final String memberId, final String lastEventId);
+
+    /**
+     * 실시간 알림 보내기
+     */
+    void send(final Notification notification);
+
+    /**
+     * 알림 생성
+     */
     void save(final Notification notification);
 
-    void send(final Notification notification);
+    /**
+     * 알림 조회
+     */
+    FindNotificationResponse find(final String memberId);
+
+    /**
+     * 알림 수정
+     */
+    void update(final List<UpdateNotificationRequest> request);
 }
