229d7af493f01beb6296cdc70080b3d29bcdcf44
fakerdeft <whakswp123@naver.com>
2024-04-16 01:16:23 +0900

    (#213)feat: 관리자용 회원 검색 메서드 구현 중입니다.

--- src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
+++ src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
@@ -2,12 +2,17 @@ package com.coverflow.member.infrastructure;
 
 import com.coverflow.member.domain.Member;
 import com.coverflow.member.dto.request.FindMemberAdminRequest;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public Optional<Page<Member>> findWithFilters(Pageable pageable, FindMemberAdminRequest request) {
