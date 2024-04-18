6eed9751dfe5d176bea8ccc2e6dd8d1b612ea318
fakerdeft <whakswp123@naver.com>
2024-04-18 21:36:45 +0900

    (#213)feat: 관리자용 회원 검색 메서드 구현 중입니다.

--- src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
+++ src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
@@ -2,10 +2,15 @@ package com.coverflow.member.infrastructure;
 
 import com.coverflow.member.domain.Member;
 import com.coverflow.member.dto.request.FindMemberAdminRequest;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.EntityPathBase;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 
@@ -13,6 +18,26 @@ import java.util.Optional;
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    public static <T> OrderSpecifier[] makeOrderSpecifiers(final EntityPathBase<T> qClass, final Pageable pageable) {
+        return pageable.getSort()
+                .stream()
+                .map(sort -> toOrderSpecifier(qClass, sort))
+                .toList().toArray(OrderSpecifier[]::new);
+    }
+
+    private static <T> OrderSpecifier toOrderSpecifier(final EntityPathBase<T> qClass, final Sort.Order sortOrder) {
+        final Order orderMethod = toOrder(sortOrder);
+        final PathBuilder<T> pathBuilder = new PathBuilder<>(qClass.getType(), qClass.getMetadata());
+        return new OrderSpecifier(orderMethod, pathBuilder.get(sortOrder.getProperty()));
+    }
+
+    private static Order toOrder(final Sort.Order sortOrder) {
+        if (sortOrder.isAscending()) {
+            return Order.ASC;
+        }
+        return Order.DESC;
+    }
 
     @Override
     public Optional<Page<Member>> findWithFilters(Pageable pageable, FindMemberAdminRequest request) {
