32be7985c3b788171ad2c940e4b86530c878ac65
fakerdeft <whakswp123@naver.com>
2024-04-19 22:53:57 +0900

    (#213)feat: 관리자용 회원 검색 메서드 구현 중입니다.

--- src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
+++ src/main/java/com/coverflow/member/infrastructure/MemberRepositoryCustomImpl.java
@@ -4,15 +4,24 @@ import com.coverflow.member.domain.Member;
 import com.coverflow.member.dto.request.FindMemberAdminRequest;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static com.coverflow.company.domain.QCompany.company;
+import static com.coverflow.member.domain.QMember.member;
 
 @RequiredArgsConstructor
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
@@ -40,7 +49,50 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
     }
 
     @Override
-    public Optional<Page<Member>> findWithFilters(Pageable pageable, FindMemberAdminRequest request) {
-        return Optional.empty();
+    public Optional<Page<Member>> findWithFilters(
+            final Pageable pageable,
+            final FindMemberAdminRequest request
+    ) {
+        CompletableFuture<List<Member>> membersFuture = CompletableFuture.supplyAsync(() ->
+                jpaQueryFactory
+                        .selectFrom(member)
+                        .where(
+
+                        )
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .orderBy(makeOrderSpecifiers(member, pageable))
+                        .fetch()
+        );
+
+        CompletableFuture<Long> countFuture = CompletableFuture.supplyAsync(() ->
+                jpaQueryFactory
+                        .select(member.count())
+                        .from(member)
+                        .where(
+
+                        )
+                        .fetchOne()
+        );
+
+        CompletableFuture.allOf(membersFuture, countFuture).join(); // 이 호출로 두 쿼리가 완료될 때까지 대기
+
+        List<Member> members;
+        long total;
+        try {
+            members = membersFuture.get();
+            total = countFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.of(new PageImpl<>(members, pageable, total));
+    }
+
+    private BooleanExpression toContainsCreatedStartDate(final String createdStartDate) {
+        if (!StringUtils.hasText(createdStartDate)) {
+            return null;
+        }
+        return member..contains(createdStartDate);
     }
 }
