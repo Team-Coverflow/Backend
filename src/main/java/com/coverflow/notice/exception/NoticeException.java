4d186fbdc6f4c408adec17c29853a5426227e766
fakerdeft <whakswp123@naver.com>
2024-04-20 16:47:00 +0900

    (#224)feat: 공지 예외 구현했습니다.

--- src/main/java/com/coverflow/notice/exception/NoticeException.java
+++ src/main/java/com/coverflow/notice/exception/NoticeException.java
@@ -1,4 +1,19 @@
 package com.coverflow.notice.exception;
 
-public class NoticeException {
+public class NoticeException extends RuntimeException {
+
+    public NoticeException(final String message) {
+        super(message);
+    }
+
+    public static class NoticeNotFoundException extends NoticeException {
+
+        public NoticeNotFoundException() {
+            super("공지가 존재하지 않습니다.");
+        }
+
+        public NoticeNotFoundException(final Object data) {
+            super(String.format("공지가 존재하지 않습니다. - request info => %s", data));
+        }
+    }
 }
