918853df802b1ae6d03f714dcb4329d43c8ac208
fakerdeft <whakswp123@naver.com>
2024-04-25 23:57:10 +0900

    (#233)feat: 무한 스크롤 기능 구현 중입니다.

--- src/main/java/com/coverflow/notification/infrastructure/NotificationCustomRepositoryImpl.java
+++ src/main/java/com/coverflow/notification/infrastructure/NotificationCustomRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.coverflow.notification.infrastructure;
 
 import com.coverflow.notification.domain.Notification;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -8,11 +9,22 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.coverflow.notification.domain.QNotification.notification;
+
 @RequiredArgsConstructor
 public class NotificationCustomRepositoryImpl implements NotificationCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public Optional<List<Notification>> findByMemberId(final UUID memberId, final LocalDateTime date) {
-        
+        List<Notification> notifications = jpaQueryFactory
+                .select(notification)
+                .where()
+                .orderBy(notification.createdAt.desc())
+                .limit(10)
+                .fetch();
+
         return Optional.empty();
     }
 }
