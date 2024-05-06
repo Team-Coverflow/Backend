5506ed415728a3ff46326933983b55ed74cdf321
fakerdeft <whakswp123@naver.com>
2024-05-07 02:16:05 +0900

    fix: 글자 수 범위 버그 수정했습니다.

--- src/main/java/com/coverflow/question/dto/request/UpdateAnswerRequest.java
+++ src/main/java/com/coverflow/question/dto/request/UpdateAnswerRequest.java
@@ -1,12 +1,12 @@
 package com.coverflow.question.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.Length;
 
 public record UpdateAnswerRequest(
 
         @NotBlank
-        @Range(min = 1, max = 500)
+        @Length(min = 1, max = 500)
         String content,
         @NotBlank
         boolean answerStatus
