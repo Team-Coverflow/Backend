4c06c7b81f88d079c9afaf9c58a26a3edc81b016
fakerdeft <whakswp123@naver.com>
2024-05-07 01:29:23 +0900

    fix: 글자 수 범위 버그 수정했습니다.

--- src/main/java/com/coverflow/question/dto/request/UpdateQuestionRequest.java
+++ src/main/java/com/coverflow/question/dto/request/UpdateQuestionRequest.java
@@ -1,12 +1,15 @@
 package com.coverflow.question.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
 
 public record UpdateQuestionRequest(
 
         @NotBlank
+        @Range(min = 1, max = 100)
         String title,
         @NotBlank
+        @Range(min = 1, max = 500)
         String content,
         boolean questionStatus
 ) {
