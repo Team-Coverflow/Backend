9a4f14df3e8fbac97d1e22b2c0cfcb60ad0ef4f4
fakerdeft <whakswp123@naver.com>
2024-05-15 01:24:31 +0900

    fix: ec2 응답 시 날짜 테스트 중입니다.

--- src/main/java/com/coverflow/CoverflowApplication.java
+++ src/main/java/com/coverflow/CoverflowApplication.java
@@ -1,11 +1,14 @@
 package com.coverflow;
 
 import com.coverflow.global.config.CorsProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @EnableAspectJAutoProxy
@@ -15,6 +18,11 @@ public class CoverflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoverflowApplication.class, args);
+    }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
